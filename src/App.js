f3d95bacd0c9f94cd4df16448742b6236af5c2fc
ArupGhoost <arupbehera243@gmail.com>
2021-03-18 23:51:50 +0530

    scrolldown

---
+++ src/App.js
@@ -0,0 +1,43 @@
+
+import './App.css';
+
+function App() {
+  return (
+    
+        
+  
+        <>
+        <div className="App"></div>
+            <div className='main'>
+                <h3>Hii </h3>
+                <h3>Im Arup</h3>
+                <h3>A Front-End Developer </h3>
+                <h1> Scroll Down</h1>
+            </div>
+            <div className='main-sec'>
+                 <h3>I'm Good At:</h3>
+                 <li>HTML</li>
+                 <li>CSS</li>
+                 <li>JavaScript</li>
+                 <li>React</li>
+                 <li>Bootstrap</li>
+                 <li>SASS(Ongoing)</li>
+            </div>
+            <div className='main-thrd'>
+               <h3>I'm a Self-Taught Developer</h3>
+               <h4>I have 3months of developing Knowledge</h4>
+              <h5>I am a quick learner(some says) and hard-working fellow</h5>
+              <h6>Rather than web developing I would love to know app dev too</h6>
+              <h2>I love Bikes and a Football fan</h2>
+            </div>
+            <div className='main-frth'>
+                <h1>Thanks For Watching</h1>
+                <h6>A better profile is coming soon :)</h6>
+            </div>
+        </>
+    )
+}
+
+
+
+export default App;
